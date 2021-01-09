39baa8a22df88019c074e78f0765968fa1e42676
Bluup-Aomine <vincent-capek@laposte.net>
2021-01-09 11:45:48 +0100

    feat: init home page

--- assets/js/components/Page/Home.svelte
+++ assets/js/components/Page/Home.svelte
@@ -26,3 +26,29 @@
         </div>
     </div>
 </div>
+<div class="company_info">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-5 col-md-5">
+                <div class="man_img_thumb">
+                    <img alt="img man thumb" src="/images/man_thumb.png">
+                </div>
+            </div>
+            <div class="col-xl-7 col-md-7">
+                <div class="company_info_text">
+                    <h3>
+                        We are an SEO company that
+                        <br>
+                        specializes in developing.
+                    </h3>
+                    <p>
+                        Esteem spirit temper too say adieus who direct esteem. It esteems luckily or picture placing
+                        drawing. Apartments frequently or motionless on reasonable.
+                        Steem spirit temper too say adieus who direct esteem.
+                    </p>
+                    <a href="/" class="boxed-btn">About Us</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
