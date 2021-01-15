41aaac84b1aad00888d0f1a03b8a6c50d378660c
Bluup-Aomine <vincent-capek@laposte.net>
2021-01-15 15:27:26 +0100

    feat: add components for dashboard page

--- assets/js/components/Admin/Page/AdminPage.svelte
+++ assets/js/components/Admin/Page/AdminPage.svelte
@@ -1,6 +1,8 @@
 <script>
     import Sidebar from "../Layout/Sidebar.svelte";
     import Navbar from "../Layout/Navbar.svelte";
+    import DashboardDataItems from "./dashboard/DashboardDataItems.svelte";
+    import BarData from "./dashboard/BarData.svelte";
 </script>
 
 <div>
@@ -9,7 +11,16 @@
         <Navbar />
         <main class="main-content background-grey-light">
             <div id="mainContent">
-
+                <div class="row gap-20 position-relative">
+                    <div class="w-100" style="position: absolute; left: 0; top: 0">
+                        <div class="row gap-20">
+                            <DashboardDataItems/>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <BarData/>
+                    </div>
+                </div>
             </div>
         </main>
     </div>
---
+++ assets/js/components/Admin/Page/dashboard/DashboardDataItems.svelte
@@ -0,0 +1,92 @@
+<script>
+    // COMPONENTS SVELTE
+    import {onMount} from "svelte";
+    // COMPONENTS LIB
+    import sparkline from "@fnando/sparkline";
+
+    onMount(() => {
+        let sparkline_element = document.querySelectorAll('.sparkline')
+        if (sparkline_element) {
+            sparkline_element.forEach((element) => {
+                sparkline(element, [1, 5, 6, 9, 10, 2, 13])
+            })
+        }
+    })
+</script>
+
+<div class="col-md-3">
+    <div class="layers bd bgc-white p-20">
+        <div class="layer w-100 mb-10">
+            <h6 class="lh-1">Total Projects</h6>
+        </div>
+        <div class="layer w-100">
+            <div class="blocks flex-nowrap">
+                <div class="block block-greed">
+                    <svg class="sparkline" width="100" height="30" stroke-width="3"></svg>
+                </div>
+                <div class="block">
+                    <span class="color-green bcg-green border-radius-10em lh-0 fw-600 px-15 py-15 vertical-align-middle d-inline-block">
+                        +10%
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="col-md-3">
+    <div class="layers bd bgc-white p-20">
+        <div class="layer w-100 mb-10">
+            <h6 class="lh-1">Total Categories</h6>
+        </div>
+        <div class="layer w-100">
+            <div class="blocks flex-nowrap">
+                <div class="block block-greed">
+                    <svg class="sparkline" width="100" height="30" stroke-width="3"></svg>
+                </div>
+                <div class="block">
+                    <span class="color-green bcg-green border-radius-10em lh-0 fw-600 px-15 py-15 vertical-align-middle d-inline-block">
+                        +10%
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="col-md-3">
+    <div class="layers bd bgc-white p-20">
+        <div class="layer w-100 mb-10">
+            <h6 class="lh-1">Total Tags</h6>
+        </div>
+        <div class="layer w-100">
+            <div class="blocks flex-nowrap">
+                <div class="block block-greed">
+                    <svg class="sparkline" width="100" height="30" stroke-width="3"></svg>
+                </div>
+                <div class="block">
+                    <span class="color-green bcg-green border-radius-10em lh-0 fw-600 px-15 py-15 vertical-align-middle d-inline-block">
+                        +10%
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="col-md-3">
+    <div class="layers bd bgc-white p-20">
+        <div class="layer w-100 mb-10">
+            <h6 class="lh-1">Total Images</h6>
+        </div>
+        <div class="layer w-100">
+            <div class="blocks flex-nowrap">
+                <div class="block block-greed">
+                    <svg class="sparkline" width="100" height="30" stroke-width="3"></svg>
+                </div>
+                <div class="block">
+                    <span class="color-green bcg-green border-radius-10em lh-0 fw-600 px-15 py-15 vertical-align-middle d-inline-block">
+                        +10%
+                    </span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
